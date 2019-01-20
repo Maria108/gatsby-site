0735c1a3c5cb4903656636a9e1c55e91e5db1d60
Maria108 <maria.abash@gmail.com>
2019-01-20 15:30:51 -0500

    Prepare

--- src/components/prepare.js
+++ src/components/prepare.js
@@ -3,11 +3,12 @@ import React from 'react'
 import '../styles/about.css'
 import { Row, Col, Grid } from 'react-bootstrap'
 
-import { FaRegCheckSquare } from 'react-icons/fa'
+import { FaCircle, FaCheck } from 'react-icons/fa'
+import { IoMdClose } from 'react-icons/io'
 
 const Prepare = () => (
   <Grid id="how-to-prepare">
-    <div className="main-div-colon picture-about">
+    <div className="main-div-colon picture-include">
       <div className="opacity-dive">
         <Row>
           <Col xs={4} md={4} />
@@ -16,58 +17,145 @@ const Prepare = () => (
               <Row>
                 <Col xs={12} md={12}>
                   <div className="about-colon">
-                    <h1>HOW TO PREPARE</h1>
+                    <p>HOW TO PREPARE</p>
+                  </div>
+                  <div className="recommendations">
+                    <p>
+                      Here are some recommendations to help you get the most out
+                      of your session, not requirements.
+                    </p>
+                    <p>Few days prior and right after your colonic session:</p>
                   </div>
                 </Col>
-                <Col xs={6} md={6}>
-                  <div>
-                    <p className="info-about-colon">What to do:</p>
-                    <p className="info-about-colon">
-                      <FaRegCheckSquare /> Stay properly hydrated.
+                <Col xs={5} md={5}>
+                  <div className="avoid">
+                    <p className="info-about-colon-dos">
+                      <span className="icon-dont">
+                        <IoMdClose />
+                      </span>
+                      AVOID
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> Drink plenty of water and green
-                      vegetable juices.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Alcohol
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> it'll help to hydrate the body ad
-                      loosen the waste.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Carbonated drinks
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> Avoid caffein, alcohol and tobacco.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Dairy Products
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> s it dehydrates the body and creates
-                      inflammation.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Fatty and fried foods
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      White flour products
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Processed and junk foods
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Refined sugar
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Acidic foods
                     </p>
                   </div>
                 </Col>
-                <Col xs={6} md={6}>
-                  <div>
-                    <p className="info-about-colon">What not to do:</p>
-                    <p className="info-about-colon">
-                      <FaRegCheckSquare /> Stay properly hydrated.
+                <Col xs={7} md={7}>
+                  <div className="include">
+                    <p className="info-about-colon-dos">
+                      {' '}
+                      <span className="icon-do">
+                        <FaCheck />
+                      </span>
+                      INCLUDE
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> Drink plenty of water and green
-                      vegetable juices.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Leafy greens (kale, spinach, mustard greens)
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> it'll help to hydrate the body ad
-                      loosen the waste.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Colorful veggies, fruits, berries
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> Avoid caffein, alcohol and tobacco.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Organic vegetable soups
                     </p>
                     <p className="info-about-colon">
-                      <FaRegCheckSquare /> s it dehydrates the body and creates
-                      inflammation.
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Veggie juices
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Whole grains (brown rice, quinoa)
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Seasonal organic vegetables
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Purified and distilled water, herbal teas
+                    </p>
+                    <p className="info-about-colon">
+                      <span className="icon">
+                        <FaCircle />
+                      </span>{' '}
+                      Alkaline foods
                     </p>
                   </div>
                 </Col>
-                <div className="mk-app-btn">
-                  <button className="app-btn">BOOK NOW</button>
-                </div>
+                <Col xs={12} md={12}>
+                  <div className="recommendations">
+                    <p>
+                      On the day of your session you should come on empty
+                      stomach, stop eating at least 3,5 hours leading up to your
+                      appointment and stop drinking about 1,5 hours before your
+                      appointment. You'll feel a lot more comfortable if you'll
+                      avoid a full stomach and bladder. It's important to stay
+                      hydrated pre and post colonic session.
+                    </p>
+                  </div>
+                </Col>
               </Row>
             </div>
           </Col>
